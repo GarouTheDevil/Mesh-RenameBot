29e60760e2db2afbcbc4f0852221f425656cf9de
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2022-02-03 09:46:59 +0530

    Update en.py

--- MeshRenameBot/translations/en.py
+++ MeshRenameBot/translations/en.py
@@ -1,90 +1,88 @@
 class EnTrans:
     
-    WRONG_VALUE_ERROR = "Invalid value entered for {} variable."
+    WRONG_VALUE_ERROR = "Invalid Value Entered For {} Variable"
     
-    START_MSG = "Hello there, I am Mesh Rename Bot. Open Source at https://github.com/yash-dk/Mesh-RenameBot/tree/master. You can deploy your own."
+    START_MSG = "Hello , I Am A Rename Bot"
      
-    CANCEL_MESSAGE = "The rename has been canceled. Will be updated soon."
+    CANCEL_MESSAGE = "The Rename Has Been Canceled"
     
-    RENAME_NO_FILTER_MATCH = "NO FILTER MATCHED ABORTING RENAME \nUsing the filters to Rename. As no name was specified. Manage your Filters using /filters."
+    RENAME_NO_FILTER_MATCH = "No Filter Matched Aborting Rename \nUsing The Filters To Rename As No Name Was Specified Manage Your Filters Using Cammand : /filters"
 
-    RENAME_FILTER_MATCH_USED = "Using the filters to Rename. As no name was specified. Manage your Filters using /filters"
+    RENAME_FILTER_MATCH_USED = "Using The Filters To Rename As No Name Was Specified Manage Your Filters Using /filters"
 
-    RENAME_NOFLTR_NONAME = "Enter the rename file name in format :- ```/rename my new filename.extension``` or ```Use /filters to set some rename filters.```"
+    RENAME_NOFLTR_NONAME = "Enter The Rename File Name In Format :- ```/rename NewFileName.format``` or ```Use /filters To Set Some Rename filters```"
 
-    RENAME_CANCEL = "Cancel this rename."
+    RENAME_CANCEL = "Cancel The Rename"
 
-    RENAMING_FILE = "Renaming the file currently hold on."
+    RENAMING_FILE = "Renaming... ⏳"
     
-    DL_RENAMING_FILE = "Downloading the file currently hold on."
+    DL_RENAMING_FILE = "Downloading...⬇️"
 
-    RENAME_ERRORED_REPORT = "The download was errored. Report this error."
+    RENAME_ERRORED_REPORT = "Error Occurred"
 
-    RENAME_CANCEL_BY_USER = "Canceled by the user."
+    RENAME_CANCEL_BY_USER = "Canceled By User"
 
-    FLTR_ADD_LEFT_STR = "Addition Filter: <code>{}</code> <code>To Left</code>"
-    FLTR_ADD_RIGHT_STR = "Addition Filter: <code>{}</code> <code>To Right</code>"
-    FLTR_RM_STR = "Remove Filter: <code>{}</code>"
-    FLTR_REPLACE_STR = "Replace Filter: <code>{}</code> with <code>{}</code>"
+    FLTR_ADD_LEFT_STR = "Addition Filter : <code>{}</code> <code> To Left</code>"
+    FLTR_ADD_RIGHT_STR = "Addition Filter : <code>{}</code> <code> To Right</code>"
+    FLTR_RM_STR = "Remove Filter : <code>{}</code>"
+    FLTR_REPLACE_STR = "Replace Filter : <code>{}</code> With <code>{}</code>"
 
-    CURRENT_FLTRS = "Your Current Filters:-"
-    ADD_FLTR = "Add Filter."
-    RM_FLTR = "Remove Filter."
+    CURRENT_FLTRS = "Your Current Filters :"
+    ADD_FLTR = "Add Filter"
+    RM_FLTR = "Remove Filter"
 
     FILTERS_INTRO = """
-Welcome to adding filter.
-3 Types of filter.
+Filters :
 
-Replace Filter:- This filter will replace a
-given word with the one you sepcified
-
-Addition Filter:- This filter will add given word
-at end or beginning.
-
-Remove Filter:- This filer will remove given word
-from the while file name.
+• Replace Filter : This Filter Will Replace A
+Given Word With The One You Specified
+• Addition Filter : This Filter Will Add Given Word
+At End Or Beginning
+•Remove Filter : This Filter Will Remove Given Word
+From The While File Name
 
 """
 
-    ADD_REPLACE_FLTR = "Add Replace Filter."
-    ADD_ADDITION_FLTR = "Add Addition Filter."
-    ADD_REMOVE_FLTR = "Add Remove Filter."
-    BACK = "Back."
+    ADD_REPLACE_FLTR = "Add Replace Filter"
+    ADD_ADDITION_FLTR = "Add Addition Filter"
+    ADD_REMOVE_FLTR = "Add Remove Filter"
+    BACK = "Back"
 
-    REPALCE_FILTER_INIT_MSG = "Send the msg in this format. <code>what to replace | what to replace with</code> or /ignore to go back.\nNote that sapce after and before '|' will be considered."
+    REPALCE_FILTER_INIT_MSG = "Send The Message In This Format : <code>What To Replace | What To Replace With</code> \n/ignore To Go Back \nNote : Space After And Before '|' Will Be Considered"
 
-    NO_INPUT_FROM_USER = "No input received from you."
-    INPUT_IGNORE = "Received ignore from you."
-    WRONG_INPUT_FORMAT = "The input is not valid. Check the format which is given."
-    REPLACE_FILTER_SUCCESS = "Added the Replace filter successfully. <code>'{}'</code> will be replaced with <code>'{}'</code>."
+    NO_INPUT_FROM_USER = "No Input Received From You"
+    INPUT_IGNORE = "Ignored Successfully"
+    WRONG_INPUT_FORMAT = "The Input Is Not Valid , Check The Format Which Is Given"
+    REPLACE_FILTER_SUCCESS = "Added The Replace Filter Successfully <code>' {} '</code> Will Be Replaced With <code>' {}'</code>"
 
-    ADDITION_FILTER_INIT_MSG = "Enter the text that you want to add or /ignore to go back."
+    ADDITION_FILTER_INIT_MSG = "Enter The Text That You Want To Add Or Use Cammand : /ignore To Go Back"
 
-    ADDITION_FILTER_SUCCESS_LEFT = "Added the Addition filter successfully. <code>{}</code> will be added to <code>LEFT</code>."
+    ADDITION_FILTER_SUCCESS_LEFT = "Added The Addition Filter Successfully <code>{}</code> Will Be Added To <code>LEFT</code>"
 
-    ADDITION_FILTER_SUCCESS_RIGHT = "Added the Addition filter successfully. <code>{}</code> will be added to <code>RIGHT</code>."
+    ADDITION_FILTER_SUCCESS_RIGHT = "Added The Addition Filter Successfully <code>{}</code> Will Be Added To <code>RIGHT</code>"
 
-    ADDITION_LEFT = "Addition to LEFT."
-    ADDITION_RIGHT = "Addition to RIGHT."
+    ADDITION_LEFT = "Addition To LEFT"
+    ADDITION_RIGHT = "Addition To RIGHT"
 
-    ADDITION_POSITION_PROMPT = "Where do you want to add the text."
+    ADDITION_POSITION_PROMPT = "Where Do You Want To Add The Text"
 
-    REMOVE_FILTER_INIT_MSG = "Enter the text that you want to remove or /ignore to go back."
+    REMOVE_FILTER_INIT_MSG = "Enter The Text That You Want To Remove Or Use Cammand : /ignore To Go Back"
 
-    REMOVE_FILTER_SUCCESS = "Added the Remove filter successfully. <code>{}</code> will be removed."
+    REMOVE_FILTER_SUCCESS = "Added The Remove Filter Successfully <code>{}</code> Will Be Removed"
 
-    REPLY_TO_MEDIA = "Reply /rename to a media file."
+    REPLY_TO_MEDIA = "Reply /rename To A Media File"
 
     HELP_STR = """
-`/start` - Check if the bot is running.
-`/rename` - Reply to media to rename `/rename filename.extension`. If only `/rename` is used filters will be used.
-`/filters` - Add/Remove Filters. Use this command to see what are these.
-`/setthumb` - Reply to image to set the thumbnail permanently.
-`/getthumb` - Get the thumbnail which is currently set.
-`/clrthumb` - Remove the thumbnail which is set.
-`/mode` - Change between 3 modes:-
-    - Same format as it was sent. [If doc is sent doc is uploaded if video is sent video is uploaded.]
-    - Force to Document. [Everything is uploaded as a file.]
-    - Upload general media. [In streamable video/audio. etc.]
-`/queue` - Gives the state of your rename and the load on bot.
+Available Cammands :
+
+• /rename - Reply To Media To Rename `/rename filename.extension` Or Use /rename For More With Filters
+• /filters - Add /Remove Filters Use this Cammad To See What Are These
+• /setthumb - Reply To Image To Set The Thumbnail Permanently
+• /getthumb - Get The Thumbnail Which Is Currently Set
+• /clrthumb - Remove The Thumbnail Which Is Set
+• /mode - Change between 3 modes:-
+    - Same Format As It Was Sent [If doc is sent doc is uploaded if video is sent video is uploaded.]
+    - Force to Document [Everything is uploaded as a file.]
+    - Upload General Media [In streamable video/audio. etc.]
+• /queue - Gives The State Of Your Rename And The Load
     """
